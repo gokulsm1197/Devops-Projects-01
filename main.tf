d1cf74adc82163d7d612768e8db2539aec1b0227
gokulsm1197 <gokulsm1197@gmail.com.com>
2024-04-04 00:07:30 +0530

    fat commit

--- main.tf
+++ main.tf
@@ -24,5 +24,43 @@ module "vpc" {
 
 # sg
 
+module "sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "jenkins_sg"
+  description = "security group for jenkins server"
+  vpc_id      = module.vpc.default_vpc_id
+
+  
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+  egress_with_cidr_blocks = [
+   {
+    from_port = 0
+    to_port = 0
+    protocol ="-1"
+    cidr_blocks ="0.0.0.0/0"
+   }
+  ]
+
+  tags = {
+    name = "jenkins-sg"
+  }
+}
+
 
 # ec2
